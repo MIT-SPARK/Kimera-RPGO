f71d3df7decad6b1d53c3049e2092e13518699e9
yunchang <yunchang@mit.edu>
2019-06-11 19:01:51 -0400

    added graph utils

---
+++ include/graph_utils/graph_types.h
@@ -0,0 +1,86 @@
+// Copyright (C) 2019 by Pierre-Yves Lajoie <lajoie.py@gmail.com>
+
+#ifndef GRAPH_UTILS_TYPES_H
+#define GRAPH_UTILS_TYPES_H
+
+#include <gtsam/base/Value.h>
+#include <gtsam/geometry/Pose3.h>
+#include <gtsam/geometry/Rot3.h>
+#include <gtsam/slam/BetweenFactor.h>
+
+#include <map>
+#include <vector>
+
+/** \namespace graph_utils
+ *  \brief This namespace encapsulates utility functions to manipulate graphs
+ */
+namespace graph_utils {
+
+/** \struct PoseWithCovariance
+ *  \brief Structure to store a pose and its covariance data
+ */
+struct PoseWithCovariance {
+    gtsam::Pose3 pose;
+    gtsam::Matrix covariance_matrix;
+};
+
+/** \struct Transform
+ *  \brief Structure defining a transformation between two poses
+ */
+struct Transform {
+    gtsam::Key i, j;
+    graph_utils::PoseWithCovariance pose;
+    bool is_separator;
+};
+
+/** \struct Transforms
+ *  \brief Structure defining a std::map of transformations
+ */
+struct Transforms {
+    gtsam::Key start_id, end_id;
+    std::map<std::pair<gtsam::Key,gtsam::Key>, graph_utils::Transform> transforms;
+};
+
+/** \struct TrajectoryPose
+ *  \brief Structure defining a pose in a robot trajectory
+ */
+struct TrajectoryPose {
+    gtsam::Key id;
+    graph_utils::PoseWithCovariance pose;
+};
+
+/** \struct Trajectory
+ *  \brief Structure defining a robot trajectory
+ */
+struct Trajectory {
+    gtsam::Key start_id, end_id;
+    std::map<gtsam::Key, graph_utils::TrajectoryPose> trajectory_poses;
+};
+
+/** \typedef LoopClosures
+ *  \brief type to store poses IDs of loop closures.
+ */
+/** Type defining a list of pair of poses with a loop closure */
+typedef std::vector<std::pair<gtsam::Key,gtsam::Key>> LoopClosures;
+
+/** \typedef ConsistencyErrorData
+ *  \brief type to store pose error vector and its associated covariance.
+ */
+/** Type defining a pair vector-matrix */
+typedef std::pair<gtsam::Vector6, gtsam::Matrix> ConsistencyErrorData;
+
+/** \var FIXED_COVARIANCE
+ * \brief Covariance matrix with usual value (rotation std: 0.01 rad, translation std: 0.1 m).
+ *
+ * This value should not be used if covariance data is provided by the front end.
+ */
+const gtsam::Matrix FIXED_COVARIANCE =
+        (Eigen::MatrixXd(6,6)  <<   0.0001, 0,      0,      0,     0,     0,
+                                    0,      0.0001, 0,      0,     0,     0,
+                                    0,      0,      0.0001, 0,     0,     0,
+                                    0,      0,      0,      0.01,  0,     0,
+                                    0,      0,      0,      0,     0.01,  0,
+                                    0,      0,      0,      0,     0,     0.01).finished();
+
+}
+#endif
---
+++ src/graph_utils/graph_utils_functions.cpp
@@ -0,0 +1,142 @@
+// Copyright (C) 2018 by Pierre-Yves Lajoie <lajoie.py@gmail.com>
+
+#include "graph_utils/graph_utils_functions.h"
+
+#include <fstream>
+#include <iostream>
+#include <math.h>
+
+namespace graph_utils {
+
+void poseCompose(const graph_utils::PoseWithCovariance &a,
+                 const graph_utils::PoseWithCovariance &b,
+                 graph_utils::PoseWithCovariance &out) {
+  gtsam::Matrix Ha, Hb;
+  out.pose = a.pose.compose(b.pose, Ha, Hb);
+  gtsam::Matrix tau1 = a.pose.AdjointMap();
+  out.covariance_matrix = a.covariance_matrix +
+      tau1 * b.covariance_matrix * tau1.transpose(); 
+}
+
+void poseInverse(const graph_utils::PoseWithCovariance &a,
+                 graph_utils::PoseWithCovariance &out) {
+  gtsam::Matrix Ha;
+  out.pose = a.pose.inverse(Ha);
+  out.covariance_matrix = Ha * a.covariance_matrix * Ha.transpose();
+}
+
+void poseBetween(const graph_utils::PoseWithCovariance &a,
+                 const graph_utils::PoseWithCovariance &b,
+                 graph_utils::PoseWithCovariance &out){
+  gtsam::Matrix Ha, Hb;
+  out.pose = a.pose.between(b.pose, Ha, Hb); // returns between in a frame 
+
+  if (a.pose.equals(b.pose)) {
+    out.covariance_matrix = Eigen::MatrixXd::Zero(6,6);
+    return;
+  }
+
+  gtsam::Matrix tau1 = a.pose.AdjointMap();
+
+  out.covariance_matrix = tau1.inverse() * (b.covariance_matrix - a.covariance_matrix) * tau1.transpose().inverse();
+
+  bool PSD = true;
+  Eigen::LLT<Eigen::MatrixXd> lltCovar1(out.covariance_matrix); // compute the Cholesky decomp
+  if(lltCovar1.info() == Eigen::NumericalIssue){  
+    PSD = false;
+  } 
+
+  if (!PSD) { 
+    tau1 = b.pose.inverse().AdjointMap();
+    out.covariance_matrix = tau1.inverse() * (a.covariance_matrix - b.covariance_matrix) * tau1.transpose().inverse();
+    Eigen::LLT<Eigen::MatrixXd> lltCovar2(out.covariance_matrix); // compute the Cholesky decomp
+    if(lltCovar2.info() == Eigen::NumericalIssue){  
+      std::cout << "Warning: Covariance matrix between two poses not PSD" << std::endl;
+    } 
+  }
+  return;
+}
+
+Trajectory buildTrajectory(const Transforms& transforms) {
+    // Initialization
+    Trajectory trajectory;
+    trajectory.start_id = transforms.start_id;
+    trajectory.end_id = transforms.end_id;
+    size_t current_pose_id = trajectory.start_id;
+    PoseWithCovariance temp_pose, total_pose;
+
+    // Add first pose at the origin
+    TrajectoryPose current_pose;
+    // TODO: Read covariance from file or add option
+    current_pose.pose.covariance_matrix = graph_utils::FIXED_COVARIANCE;
+    current_pose.id = current_pose_id;
+    temp_pose = current_pose.pose;
+    trajectory.trajectory_poses.insert(std::make_pair(current_pose_id, current_pose));
+
+    // Initialization
+    std::pair<size_t, size_t> temp_pair = std::make_pair(current_pose_id, current_pose_id + 1);
+    auto temp_it = transforms.transforms.find(temp_pair);
+
+    // Compositions in chain on the trajectory transforms.
+    while (temp_it != transforms.transforms.end() && !(*temp_it).second.is_separator) {
+        poseCompose(temp_pose, (*temp_it).second.pose, total_pose);
+        temp_pose = total_pose;
+        current_pose_id++;
+        current_pose.id = current_pose_id;
+        current_pose.pose = total_pose;
+        trajectory.trajectory_poses.insert(std::make_pair(current_pose_id, current_pose));
+        temp_pair = std::make_pair(current_pose_id, current_pose_id + 1);
+        temp_it = transforms.transforms.find(temp_pair);
+    }
+
+    return trajectory;
+}
+
+void printConsistencyGraph(const Eigen::MatrixXi& consistency_matrix, const std::string& file_name) {
+    // Intialization
+    int nb_consistent_measurements = 0;
+    
+    // Format edges.
+    std::stringstream ss;
+    for (int i = 0; i < consistency_matrix.rows(); i++) {
+      for (int j = i; j < consistency_matrix.cols(); j++) {
+        if (consistency_matrix(i,j) == 1) {
+          ss << i+1 << " " << j+1 << std::endl;
+          nb_consistent_measurements++;
+        }
+      }
+    }
+    
+    // Write to file
+    std::ofstream output_file;
+    output_file.open(file_name);
+    output_file << "%%MatrixMarket matrix coordinate pattern symmetric" << std::endl;
+    output_file << consistency_matrix.rows() << " " << consistency_matrix.cols() << " " << nb_consistent_measurements << std::endl;
+    output_file << ss.str();
+    output_file.close();
+}
+
+bool isInTrajectory(const Trajectory& trajectory, const size_t& pose_id) {
+  return trajectory.trajectory_poses.find(pose_id) != trajectory.trajectory_poses.end();
+}
+
+void printConsistentLoopClosures(const LoopClosures& loop_closures, const std::vector<int>& max_clique_data, const std::string& file_name){
+  std::ofstream output_file;
+  output_file.open(file_name);
+  for (const auto& loop_closure_id: max_clique_data) {
+    // -1 because fast max-clique finder is one-based.
+    output_file << loop_closures[loop_closure_id-1].first << " " << loop_closures[loop_closure_id-1].second << std::endl;
+  }
+  output_file.close();
+}
+
+int findMaxClique(const Eigen::MatrixXd adjMatrix, std::vector<int>& max_clique) {
+  // Compute maximum clique
+  FMC::CGraphIO gio;
+  gio.ReadEigenAdjacencyMatrix(adjMatrix, 0.0);
+  int max_clique_size = 0;
+  max_clique_size = FMC::maxClique(gio, max_clique_size, max_clique);
+  return max_clique_size;
+}
+
+}

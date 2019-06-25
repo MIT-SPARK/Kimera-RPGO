0b4901a3c1f6bca6050c7bbeace7194f88ae1b84
bmorrell <benjamin.morrell@jpl.nasa.gov>
2019-06-25 10:59:36 -0700

    adding default argument

--- RobustPGO/GenericSolver.h
+++ RobustPGO/GenericSolver.h
@@ -40,7 +40,8 @@ public:
               gtsam::Values values=gtsam::Values(),
               gtsam::FactorIndices factorsToRemove=gtsam::FactorIndices());
 
-  void removeFactorsNoUpdate(gtsam::FactorIndices factorsToRemove);
+  void removeFactorsNoUpdate(
+      gtsam::FactorIndices factorsToRemove = gtsam::FactorIndices());
 
   gtsam::Values calculateEstimate() { return values_; }
   gtsam::Values calculateBestEstimate() { return values_; }

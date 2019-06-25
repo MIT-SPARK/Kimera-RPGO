0a5c74023eee640dddac7f7dda1339518d9bd3b7
yunchang <yunchang@mit.edu>
2019-06-25 11:06:14 -0400

    incorporated add 2d

--- examples/RpgoReadG2o.cpp
+++ examples/RpgoReadG2o.cpp
@@ -5,32 +5,39 @@ author: Yun Chang
 
 #include "RobustPGO/RobustPGO.h"
 #include "RobustPGO/pcm/pcm.h"
+#include "RobustPGO/logger.h"
 
 #include <gtsam/slam/dataset.h>
 #include <gtsam/geometry/Pose3.h>
 #include <gtsam/geometry/Pose2.h>
 
-int main(int argc, char *argv[]) {
-  const gtsam::GraphAndValues gv = gtsam::load3D(argv[1]);
-  gtsam::NonlinearFactorGraph nfg = *gv.first;
+#include <stdlib.h>
+
+/* Usage: ./RpgoReadG2o 2d <some-2d-g2o-file> <odom-threshold> <pcm-threshold>
+   [or]   ./RpgoReadG2o 3d <some-3d-g2o-file> <odom-threshold> <pcm-threshold>*/
+template<class T>
+void Simulate(gtsam::GraphAndValues gv, 
+			double odom_thresh, double pmc_thresh) {
+
+	gtsam::NonlinearFactorGraph nfg = *gv.first;
+
   gtsam::Values values = *gv.second;
 
-  OutlierRemoval *pcm = new PCM<gtsam::Pose3>(100.0, 0.5); // with debug set to true
+  OutlierRemoval *pcm = new PCM<T>(odom_thresh, pmc_thresh);
   std::unique_ptr<RobustPGO> pgo;
-  pgo.reset(new RobustPGO(pcm)); // with debug set to true
+  pgo.reset(new RobustPGO(pcm));
 
   // first fix the first with a prior factor 
   gtsam::Key current_key = nfg[0]->front();
-  gtsam::Vector6 noise;
-  noise.setConstant(10.0);
+  Eigen::VectorXd noise = Eigen::VectorXd::Zero(graph_utils::getDim<T>());
 	static const gtsam::SharedNoiseModel& init_noise = 
 			gtsam::noiseModel::Diagonal::Sigmas(noise);
 
 	gtsam::Values init_values; // add first value with prior factor 
 	gtsam::NonlinearFactorGraph init_factors; 
-	init_values.insert(current_key, values.at<gtsam::Pose3>(current_key));
-	init_factors.add(gtsam::PriorFactor<gtsam::Pose3>(current_key, 
-			values.at<gtsam::Pose3>(current_key), init_noise));
+	init_values.insert(current_key, values.at<T>(current_key));
+	init_factors.add(gtsam::PriorFactor<T>(current_key, 
+			values.at<T>(current_key), init_noise));
 	pgo->update(init_factors, init_values);
 
 	// For now assume that there are only odometry and loop closures 
@@ -41,7 +48,7 @@ int main(int argc, char *argv[]) {
   	if (front == current_key && front == back - 1) { 
   		// odometry factor 
       gtsam::Values new_values; 
-      new_values.insert(back, values.at<gtsam::Pose3>(back));
+      new_values.insert(back, values.at<T>(back));
       gtsam::NonlinearFactorGraph new_factors; 
       new_factors.add(nfg[i]);
       std::cout << "odometry: " << front << ">" << back << std::endl; 
@@ -57,4 +64,22 @@ int main(int argc, char *argv[]) {
       pgo->update(new_factors, gtsam::Values());
   	}
   }
+}
+int main(int argc, char *argv[]) {
+	gtsam::GraphAndValues graphNValues; 
+	std::string dim = argv[1];
+	if (dim == "2d") {
+		graphNValues = gtsam::load2D(argv[2]);
+		Simulate<gtsam::Pose2>(graphNValues,
+				atof(argv[3]), atof(argv[4]));
+
+	} else if (dim == "3d") {
+		graphNValues = gtsam::load3D(argv[2]);
+		Simulate<gtsam::Pose3>(graphNValues,
+				atof(argv[3]), atof(argv[4]));
+	} else {
+		log<WARNING>("Unsupported input format: ");
+		log<WARNING>("Should be ./RpgoReadG2o <2d or 3d> <g2o file> <odom thresh> <pcm thresh>");
+	}
+  
 }

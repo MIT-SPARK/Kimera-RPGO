6f9d846f92e316d6a904921293a8bb02e91296e1
user <yunchang@mit.edu>
2019-07-04 17:17:24 -0400

    some tests for pcm optimize check

---
+++ RobustPGO/pcm/tests/testDoOptimize.cpp
@@ -0,0 +1,92 @@
+/**
+ * @file    testDoOptimize.cpp
+ * @brief   Unit test for pcm and optimize conditions
+ * @author  Yun Chang
+ */
+
+#include <CppUnitLite/TestHarness.h>
+#include <random>
+
+#include "RobustPGO/pcm/pcm.h" 
+
+/* ************************************************************************* */
+TEST(DoOptimize, Odometry)
+{
+  // test that when opdemtry edge is received added but return false
+  OutlierRemoval *pcm = new PCM<gtsam::Pose3>(1.0, 1.0);
+  pcm->setQuiet();
+
+  static const gtsam::SharedNoiseModel& noise = 
+      gtsam::noiseModel::Isotropic::Variance(6, 0.01);   
+
+  gtsam::NonlinearFactorGraph nfg; 
+  gtsam::Values est; 
+
+  // initialize first (w/ prior)
+  gtsam::Values init_vals;
+  gtsam::NonlinearFactorGraph init_factors; 
+  init_vals.insert(0, gtsam::Pose3());
+  init_factors.add(gtsam::PriorFactor<gtsam::Pose3>(0, gtsam::Pose3(), noise));
+  pcm->process(init_factors, init_vals, nfg, est);
+
+  EXPECT(gtsam::assert_equal(nfg.size(), size_t(1)));
+  EXPECT(gtsam::assert_equal(est.size(), size_t(1)));
+
+  // add odometry 
+  gtsam::Values vals;
+  gtsam::NonlinearFactorGraph factors; 
+  gtsam::Pose3 odom = gtsam::Pose3(gtsam::Rot3(), gtsam::Point3(1,0,0));
+  vals.insert(1, odom);
+  factors.add(gtsam::BetweenFactor<gtsam::Pose3>(0, 1, odom, noise));
+  bool do_optimize = pcm->process(factors, vals, nfg, est);
+  EXPECT(gtsam::assert_equal(nfg.size(), size_t(2)));
+  EXPECT(gtsam::assert_equal(est.size(), size_t(2)));
+  EXPECT(do_optimize == false);
+}
+
+TEST(DoOptimize, OdometryNoPrior)
+{
+  // test that when opdemtry edge is received added but return false
+  OutlierRemoval *pcm = new PCM<gtsam::Pose3>(1.0, 1.0);
+  pcm->setQuiet();
+
+  static const gtsam::SharedNoiseModel& noise = 
+      gtsam::noiseModel::Isotropic::Variance(6, 0.01);   
+
+  gtsam::NonlinearFactorGraph nfg; 
+  gtsam::Values est; 
+
+  // initialize first (w/o prior)
+  gtsam::Values init_vals;
+  init_vals.insert(0, gtsam::Pose3());
+  pcm->process(gtsam::NonlinearFactorGraph(), init_vals, nfg, est);
+
+  EXPECT(gtsam::assert_equal(nfg.size(), size_t(0)));
+  EXPECT(gtsam::assert_equal(est.size(), size_t(1)));
+
+  // add odometry 
+  gtsam::Values vals;
+  gtsam::NonlinearFactorGraph factors; 
+  gtsam::Pose3 odom = gtsam::Pose3(gtsam::Rot3(), gtsam::Point3(1,0,0));
+  vals.insert(1, odom);
+  factors.add(gtsam::BetweenFactor<gtsam::Pose3>(0, 1, odom, noise));
+  bool do_optimize = pcm->process(factors, vals, nfg, est);
+  EXPECT(gtsam::assert_equal(nfg.size(), size_t(1)));
+  EXPECT(gtsam::assert_equal(est.size(), size_t(2)));
+  EXPECT(do_optimize == false);
+}
+/* ************************************************************************* */
+TEST(DoOptimize, LoopClosure)
+{
+  // test that when loop closure edge is received added and return true 
+}
+
+/* ************************************************************************* */
+TEST(DoOptimize, Special)
+{
+  // test that when loop with special symbol received, added and return true
+}
+
+/* ************************************************************************* */
+  int main() { TestResult tr; return TestRegistry::runAllTests(tr);}
+/* ************************************************************************* */

8b32c2b9e6b2a9afe8aa1912a2729757a617bbcf
user <yunchang@mit.edu>
2019-07-09 07:37:00 -0400

    update load and add function for consistency with lamp

--- RobustPGO/RobustPGO.h
+++ RobustPGO/RobustPGO.h
@@ -39,11 +39,76 @@ public:
 
   template<class T>
   void loadGraph(gtsam::NonlinearFactorGraph factors, gtsam::Values values,
-      gtsam::PriorFactor<T> prior);
+      gtsam::PriorFactor<T> prior) {
+    gtsam::NonlinearFactorGraph prior_factor;
+    prior_factor.add(prior);
+    gtsam::Values priot_values; 
+    priot_values.insert(prior.key(), prior.prior());
+    update(prior_factor, priot_values); // triggers initialization
+    addGraph<T>(factors, values, prior.key());
+  }
 
   template<class T>
-  void addGraph(gtsam::NonlinearFactorGraph factors, gtsam::Values values, 
-      gtsam::Key key0);
+  void addGraph(gtsam::NonlinearFactorGraph factors, gtsam::Values values, gtsam::Key key0) {
+
+    // load graph assumes that the previous graph has been cleared
+    gtsam::Key current_key = key0; // initial key
+    // note that as of now only deal with between factors)
+    // first load the odometry
+    // order a nonlinear factor graph as odometry first 
+    bool extracted_odom = false; 
+    while (!extracted_odom) {
+      bool end_of_odom = true; 
+      for (size_t i = 0; i < factors.size(); i++) { 
+        // search through 
+        if (factors[i] != NULL &&
+            factors[i]->front() == current_key && factors[i]->back() == current_key + 1) {
+          end_of_odom = false;
+          // if (debug_) {
+          //     std::cout << "odometry: " << current_key << ">" << current_key + 1 << std::endl; 
+          // }
+          gtsam::Values new_values;
+          gtsam::NonlinearFactorGraph new_factors; 
+          new_values.insert(current_key + 1, values.at<T>(current_key + 1));
+          new_factors.add(factors[i]);
+          update(new_factors, new_values);
+          current_key = current_key + 1;
+          factors[i].reset();
+          break;
+        }
+      }
+      if (end_of_odom) extracted_odom = true; 
+    }
+
+    // now search for the special symbols (i.e. artifacts)
+    for (size_t i = 0; i < factors.size(); i++) { 
+      if (factors[i] != NULL){
+        gtsam::Symbol symb(factors[i]->back());
+        if (specialSymbol(symb.chr())) {
+          gtsam::Values new_values;
+          gtsam::NonlinearFactorGraph new_factors; 
+          new_values.insert(factors[i]->back(), values.at<T>(factors[i]->back()));
+          new_factors.add(factors[i]);
+
+          update(new_factors, new_values);
+          factors[i].reset();
+          break;
+        }
+      }
+    }
+     
+    // add the other non-odom loop closures
+    for (size_t i = 0; i < factors.size(); i++) { 
+      if (factors[i] != NULL) {
+        gtsam::NonlinearFactorGraph new_factors;
+        // if (debug_) {
+        //   std::cout << "loop closure: " << factors[i]->front() << ">" << factors[i]->back() << std::endl;
+        // }
+        new_factors.add(factors[i]);
+        update(new_factors, gtsam::Values());
+      }
+    }
+  }
 
 private:
   OutlierRemoval* outlier_removal_; // outlier removal method; 
--- examples/RpgoReadG2o.cpp
+++ examples/RpgoReadG2o.cpp
@@ -17,11 +17,11 @@ author: Yun Chang
    [or]   ./RpgoReadG2o 3d <some-3d-g2o-file> <odom-threshold> <pcm-threshold> <output-g2o-file> <verbosity>*/
 template<class T>
 void Simulate(gtsam::GraphAndValues gv, 
-			double odom_thresh, double pmc_thresh, 
+      double odom_thresh, double pmc_thresh, 
       std::string output_folder, 
       bool debug=true) {
 
-	gtsam::NonlinearFactorGraph nfg = *gv.first;
+  gtsam::NonlinearFactorGraph nfg = *gv.first;
   gtsam::Values values = *gv.second;
   
   OutlierRemoval *pcm = new PCM<T>(odom_thresh, pmc_thresh);
@@ -37,7 +37,7 @@ void Simulate(gtsam::GraphAndValues gv,
   Eigen::VectorXd noise = Eigen::VectorXd::Zero(graph_utils::getDim<T>());
   static const gtsam::SharedNoiseModel& init_noise = 
       gtsam::noiseModel::Diagonal::Sigmas(noise);
-      
+
   gtsam::Key current_key = nfg[0]->front();
 
   gtsam::Values init_values; // add first value with prior factor 
@@ -45,47 +45,15 @@ void Simulate(gtsam::GraphAndValues gv,
   init_values.insert(current_key, values.at<T>(current_key));
   gtsam::PriorFactor<T> prior_factor(current_key, 
       values.at<T>(current_key), init_noise);
-  init_factors.add(prior_factor);
-  pgo->update(init_factors, init_values); // triggers initialization
 
-	// For now assume that there are only odometry and loop closures 
-  size_t num_factors = nfg.size(); 
-  for (size_t i = 0; i < num_factors; i++) { 
-  	gtsam::Key front = nfg[i]->front();
-  	gtsam::Key back = nfg[i]->back();
-  	if (front == current_key && front == back - 1) { 
-  		// odometry factor 
-      gtsam::Values new_values; 
-      new_values.insert(back, values.at<T>(back));
-      gtsam::NonlinearFactorGraph new_factors; 
-      new_factors.add(nfg[i]);
+  pgo->loadGraph(nfg, values, prior_factor);
 
-      if (debug) {
-        std::cout << "odometry: " << front << ">" << back << std::endl; 
-        nfg[i]->print();
-      }
-
-      pgo->update(new_factors, new_values);
-      current_key++; 
-    } else { 
-  		// loop closure factor 
-      gtsam::NonlinearFactorGraph new_factors; 
-      new_factors.add(nfg[i]);
-
-      if (debug) {
-        std::cout << "loop closure: " << front << ">" << back << std::endl;
-        nfg[i]->print(); 
-      }
-
-      pgo->update(new_factors, gtsam::Values());
-  	}
-  }
   // in case no loop closure, need to force optimize with only odom 
   pgo->force_optimize(); 
 }
 int main(int argc, char *argv[]) {
-	gtsam::GraphAndValues graphNValues; 
-	std::string dim = argv[1];
+  gtsam::GraphAndValues graphNValues; 
+  std::string dim = argv[1];
   std::string output_folder;
   if (argc > 5) output_folder = argv[5];
   bool verbose = false;
@@ -95,22 +63,22 @@ int main(int argc, char *argv[]) {
     if (flag == "v") verbose = true; 
   }
 
-	if (dim == "2d") {
-		graphNValues = gtsam::load2D(argv[2], gtsam::SharedNoiseModel(),
+  if (dim == "2d") {
+    graphNValues = gtsam::load2D(argv[2], gtsam::SharedNoiseModel(),
         0, false, true, gtsam::NoiseFormatG2O);
-		Simulate<gtsam::Pose2>(graphNValues,
-				atof(argv[3]), atof(argv[4]), 
+    Simulate<gtsam::Pose2>(graphNValues,
+        atof(argv[3]), atof(argv[4]), 
         output_folder, verbose);
 
-	} else if (dim == "3d") {
-		graphNValues = gtsam::load3D(argv[2]);
-		Simulate<gtsam::Pose3>(graphNValues,
-				atof(argv[3]), atof(argv[4]),
+  } else if (dim == "3d") {
+    graphNValues = gtsam::load3D(argv[2]);
+    Simulate<gtsam::Pose3>(graphNValues,
+        atof(argv[3]), atof(argv[4]),
         output_folder, verbose);
 
-	} else {
-		log<WARNING>("Unsupported input format: ");
-		log<WARNING>("Should be ./RpgoReadG2o <2d or 3d> <g2o file> <odom thresh> <pcm thresh> <opt: output_folder> <opt: v for messages");
-	}
+  } else {
+    log<WARNING>("Unsupported input format: ");
+    log<WARNING>("Should be ./RpgoReadG2o <2d or 3d> <g2o file> <odom thresh> <pcm thresh> <opt: output_folder> <opt: v for messages");
+  }
   
 }

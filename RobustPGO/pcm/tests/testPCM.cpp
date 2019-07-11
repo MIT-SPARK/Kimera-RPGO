88386ac293c914af8528e8ea3c7cda80a42188d7
user <yunchang@mit.edu>
2019-07-11 09:47:23 -0400

    add todos for 2d covar check

--- RobustPGO/pcm/tests/testPCM.cpp
+++ RobustPGO/pcm/tests/testPCM.cpp
@@ -179,70 +179,13 @@ TEST(PCM, ConsistencyCheck)
 /* ************************************************************************* */
 TEST(PCM, OdometryCheck2D)
 {
-  // Here want to test carefully pcm 
-  // test that when opdemtry edge is received added but return false
-  OutlierRemoval *pcm = new PCM<gtsam::Pose3>(1.0, 1.0);
-  pcm->setQuiet();
-
-  static const gtsam::SharedNoiseModel& noise = 
-      gtsam::noiseModel::Isotropic::Variance(6, 0.01);   
-
-  gtsam::NonlinearFactorGraph nfg; 
-  gtsam::Values est; 
-
-  // initialize first (w/ prior)
-  gtsam::Values init_vals;
-  gtsam::NonlinearFactorGraph init_factors; 
-  init_vals.insert(0, gtsam::Pose3());
-  init_factors.add(gtsam::PriorFactor<gtsam::Pose3>(0, gtsam::Pose3(), noise));
-  pcm->process(init_factors, init_vals, nfg, est);
-
-  EXPECT(gtsam::assert_equal(nfg.size(), size_t(1)));
-  EXPECT(gtsam::assert_equal(est.size(), size_t(1)));
-
-  // add odometry 
-  gtsam::Values vals;
-  gtsam::NonlinearFactorGraph factors; 
-  gtsam::Pose3 odom = gtsam::Pose3(gtsam::Rot3(), gtsam::Point3(1,0,0));
-  vals.insert(1, odom);
-  factors.add(gtsam::BetweenFactor<gtsam::Pose3>(0, 1, odom, noise));
-  bool do_optimize = pcm->process(factors, vals, nfg, est);
-  EXPECT(gtsam::assert_equal(nfg.size(), size_t(2)));
-  EXPECT(gtsam::assert_equal(est.size(), size_t(2)));
-  EXPECT(do_optimize == false);
+  // TODO
 }
 
 /* ************************************************************************* */
 TEST(PCM, ConsistencyCheck2D)
 {
-  // test that when opdemtry edge is received added but return false
-  OutlierRemoval *pcm = new PCM<gtsam::Pose3>(1.0, 1.0);
-  pcm->setQuiet();
-
-  static const gtsam::SharedNoiseModel& noise = 
-      gtsam::noiseModel::Isotropic::Variance(6, 0.01);   
-
-  gtsam::NonlinearFactorGraph nfg; 
-  gtsam::Values est; 
-
-  // initialize first (w/o prior)
-  gtsam::Values init_vals;
-  init_vals.insert(0, gtsam::Pose3());
-  pcm->process(gtsam::NonlinearFactorGraph(), init_vals, nfg, est);
-
-  EXPECT(gtsam::assert_equal(nfg.size(), size_t(0)));
-  EXPECT(gtsam::assert_equal(est.size(), size_t(1)));
-
-  // add odometry 
-  gtsam::Values vals;
-  gtsam::NonlinearFactorGraph factors; 
-  gtsam::Pose3 odom = gtsam::Pose3(gtsam::Rot3(), gtsam::Point3(1,0,0));
-  vals.insert(1, odom);
-  factors.add(gtsam::BetweenFactor<gtsam::Pose3>(0, 1, odom, noise));
-  bool do_optimize = pcm->process(factors, vals, nfg, est);
-  EXPECT(gtsam::assert_equal(nfg.size(), size_t(1)));
-  EXPECT(gtsam::assert_equal(est.size(), size_t(2)));
-  EXPECT(do_optimize == false);
+  // TODO
 }
 
 /* ************************************************************************* */

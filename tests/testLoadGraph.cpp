45a540330134068f4d7bba5b2f66be99b116f3df
yunchang <yunchang@mit.edu>
2019-07-19 11:37:58 -0400

    add test for generic solver

--- tests/testLoadGraph.cpp
+++ tests/testLoadGraph.cpp
@@ -167,6 +167,76 @@ TEST(RobustSolver, Add2)
   EXPECT(nfg_out.size()==size_t(97));
   EXPECT(values_out.size()==size_t(92));
 }
+
+/* ************************************************************************* */
+TEST(GenericSolver, Load)
+{
+  // load graph
+  // read g2o file for robot a 
+  gtsam::NonlinearFactorGraph::shared_ptr nfg;
+  gtsam::Values::shared_ptr values;
+  boost::tie(nfg, values) = gtsam::load3D(std::string(DATASET_PATH) + "/robot_a.g2o");
+
+  // set up GenericSolver
+  std::unique_ptr<GenericSolver> pgo = std::make_unique<GenericSolver>();
+
+  // Create prior
+  static const gtsam::SharedNoiseModel& noise = 
+      gtsam::noiseModel::Isotropic::Variance(6, 0.01);   
+
+  gtsam::Key init_key = gtsam::Symbol('a', 0);
+  gtsam::PriorFactor<gtsam::Pose3> init(init_key, values->at<gtsam::Pose3>(init_key), noise);
+
+  // Load graph using prior
+  pgo->loadGraph(*nfg, *values, init);
+
+  gtsam::NonlinearFactorGraph nfg_out = pgo->getFactorsUnsafe();
+  gtsam::Values values_out = pgo->calculateEstimate();
+
+  // Since thresholds are high, should have all the edges
+  EXPECT(nfg_out.size()==size_t(53));
+  EXPECT(values_out.size()==size_t(50));
+}
+
+/* ************************************************************************* */
+TEST(GenericSolver, Add)
+{
+  // load graph for robot a (same as above)
+  gtsam::NonlinearFactorGraph::shared_ptr nfg;
+  gtsam::Values::shared_ptr values;
+  boost::tie(nfg, values) = gtsam::load3D(std::string(DATASET_PATH) + "/robot_a.g2o");
+
+  // set up GenericSolver
+  std::unique_ptr<GenericSolver> pgo = std::make_unique<GenericSolver>();
+
+  static const gtsam::SharedNoiseModel& noise = 
+      gtsam::noiseModel::Isotropic::Variance(6, 0.01);   
+
+  gtsam::Key init_key = gtsam::Symbol('a', 0);
+  gtsam::PriorFactor<gtsam::Pose3> init(init_key, values->at<gtsam::Pose3>(init_key), noise);
+  pgo->loadGraph(*nfg, *values, init);// first load 
+
+  // add graph 
+  // read g2o file for robot b
+  gtsam::NonlinearFactorGraph::shared_ptr nfg_b;
+  gtsam::Values::shared_ptr values_b;
+  boost::tie(nfg_b, values_b) = gtsam::load3D(std::string(DATASET_PATH) + "/robot_b.g2o");
+
+  // create the between factor for connection
+  gtsam::Key init_key_b = gtsam::Symbol('b', 0);
+  gtsam::Pose3 transform_ab = values->at<gtsam::Pose3>(init_key).between(values_b->at<gtsam::Pose3>(init_key_b));
+  gtsam::BetweenFactor<gtsam::Pose3> bridge(init_key, init_key_b, transform_ab, noise);
+
+  // add graph 
+  pgo->addGraph(*nfg_b, *values_b, bridge);
+
+  gtsam::NonlinearFactorGraph nfg_out = pgo->getFactorsUnsafe();
+  gtsam::Values values_out = pgo->calculateEstimate();
+
+  // Since thresholds are high, should have all the edges
+  EXPECT(nfg_out.size()==size_t(97));
+  EXPECT(values_out.size()==size_t(92));
+}
 /* ************************************************************************* */
   int main() { TestResult tr; return TestRegistry::runAllTests(tr);}
 /* ************************************************************************* */

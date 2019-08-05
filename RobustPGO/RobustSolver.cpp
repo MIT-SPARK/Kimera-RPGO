2647860ea7030e84dc571e7950d136967eceaacd
marcusabate <marcus.abate98@gmail.com>
2019-08-05 15:49:18 -0400

    Move update_batch to implementation file

--- RobustPGO/RobustSolver.cpp
+++ RobustPGO/RobustSolver.cpp
@@ -133,4 +133,85 @@ void RobustSolver::addOdometry(const gtsam::NonlinearFactorGraph& odom_factor, c
   }
 }
 
+void RobustSolver::updateBatch(gtsam::NonlinearFactorGraph factors,
+    const gtsam::Values& values, const gtsam::Key& key0) {
+
+  // load graph assumes that the previous graph has been cleared
+  gtsam::Key current_key = key0; // initial key
+  // note that as of now only deal with between factors)
+  // first load the odometry
+  // order a nonlinear factor graph as odometry first
+  bool extracted_odom = false;
+  while (!extracted_odom) {
+    bool end_of_odom = true;
+    for (size_t i = 0; i < factors.size(); i++) {
+      // search through
+      if (factors[i] != NULL && factors[i]->keys().size() == 2 &&
+          factors[i]->front() == current_key && factors[i]->back() == current_key + 1) {
+        end_of_odom = false;
+
+        gtsam::Values new_values;
+        gtsam::NonlinearFactorGraph new_factors;
+        // assumes key0 is already in the graph/values
+        new_values.insert(current_key + 1, values.at(current_key + 1));
+        new_factors.add(factors[i]);
+
+        addOdometry(new_factors, new_values);
+
+        current_key = current_key + 1;
+        factors[i].reset(); // delete factor from graph
+        break;
+      }
+    }
+    if (end_of_odom) extracted_odom = true;
+  }
+
+  // now search for the special symbols (i.e. artifacts)
+  std::vector<gtsam::Key> landmarks;
+  for (size_t i = 0; i < factors.size(); i++) {
+    if (factors[i] != NULL){
+      gtsam::Symbol symb(factors[i]->back());
+      if (isSpecialSymbol(symb.chr())) {
+        // check that landmark have not previously been seen
+        if (std::find(landmarks.begin(), landmarks.end(), symb) == landmarks.end()) {
+          gtsam::Values new_values;
+          gtsam::NonlinearFactorGraph new_factors;
+          new_values.insert(factors[i]->back(), values.at(factors[i]->back()));
+          new_factors.add(factors[i]);
+          landmarks.push_back(symb);
+
+          // This is essentially addOdometry, but let's not call it that here?
+          // Since what's happening in outlier_removal_ is different
+          if (outlier_removal_) {
+            outlier_removal_->removeOutliers(new_factors, new_values, nfg_, values_);
+          } else {
+            addAndCheckIfOptimize(new_factors, new_values);
+          }
+
+          factors[i].reset();
+        }
+      }
+    }
+  }
+
+  // add the other non-odom loop closures
+  gtsam::NonlinearFactorGraph new_factors;
+  for (size_t i = 0; i < factors.size(); i++) {
+    if (factors[i] != NULL) {
+      // if (debug_) {
+      //   std::cout << "loop closure: " << factors[i]->front() << ">" << factors[i]->back() << std::endl;
+      // }
+      new_factors.add(factors[i]);
+    }
+  }
+
+  if (outlier_removal_) {
+    outlier_removal_->removeOutliers(new_factors, gtsam::Values(), nfg_, values_);
+  } else {
+    addAndCheckIfOptimize(new_factors, gtsam::Values());
+  }
+
+  optimize(); // optimize once after loading
+}
+
 }

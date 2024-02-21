b660fb9ac85d77c730ef3941cb1b82a4ecb70919
Yun Chang <yunchang@mit.edu>
2024-02-20 19:45:57 -0500

    finish visualization

--- scripts/utils.py
+++ scripts/utils.py
@@ -2,6 +2,7 @@ import csv
 import numpy as np
 import os
 from enum import Enum
+from scipy.spatial.transform import Rotation as Rot
 
 
 def split_pg_key(pg_key):
@@ -15,21 +16,53 @@ def join_pg_key(c_, j_):
 
 
 class NodeType(Enum):
-    ODOM = 0
+    POSE = 0
     LANDMARK = 1
-    VERTEX = 2
 
 
 class EdgeType(Enum):
     ODOM = 0
     LOOP_CLOSURE = 1
-    ASSOCIATION = 2
+    LANDMARK = 2
+    ASSOCIATION = 3
+
+
+class Pose2D:
+    def __init__(self, *args):
+        if len(args) == 3:
+            self.x, self.y, self.theta = args[0], args[1], args[2]
+        if len(args) == 1:
+            # parse g2o line
+            entries = args[0].split()
+            self.x = float(entries[2])
+            self.y = float(entries[3])
+            if len(entries) == 5:
+                self.theta = float(entries[4])
+            else:
+                self.theta = 0
+
+
+class Pose3D:
+    def __init__(self, *args):
+        if len(args) == 2:
+            self.t, self.R = args[0], args[1]
+        if len(args) == 1:
+            # parse g2o line
+            # VERTEX_SE3:QUAT 6989586621679009798 6.7564 -0.503973 1.89705 -0.0427225 -0.0831353 0.0310157 0.995139
+            entries = args[0].split()
+            self.t = np.array([float(e) for e in entries[2:5]])
+            if len(entries) == 9:
+                quat = np.array([float(e) for e in entries[5:9]])
+                self.R = Rot.from_quat(quat)
+            else:
+                self.R = Rot.from_quat([0, 0, 0, 1])
 
 
 class Node:
-    def __init__(self, key, node_type):
+    def __init__(self, key, node_type, pose):
         self.key = key
         self.type = node_type
+        self.pose = pose
 
 
 class Edge:
@@ -45,32 +78,69 @@ class Graph:
         self.edges = []
 
     def add_node(self, node):
-        pass
+        self.nodes[node.key] = node
 
     def add_edge(self, edge):
-        pass
+        self.edges.append(edge)
 
 
-def node_from_g2o_vertex(g2o_line):
-    if line.startswith("VERTEX_SE2"):
-        pass
-        return node
-    if line.startswith("VERTEX_SE3"):
-        pass
-        return node
+def node_from_g2o_vertex(g2o_line, is_3d):
+    if not is_3d:
+        if g2o_line.startswith("VERTEX_SE2"):
+            node_type = NodeType.POSE
+        elif g2o_line.startswith("VERTEX_XY"):
+            node_type = NodeType.LANDMARK
+        else:
+            raise Exception(
+                "Unhandled input node type {}".format(g2o_line.split()[0]))
+        key = int(g2o_line.split()[1])
+        pose = Pose2D(g2o_line)
+        return Node(key, node_type, pose)
+    else:
+        if g2o_line.startswith("VERTEX_SE3:QUAT"):
+            node_type = NodeType.POSE
+        elif g2o_line.startswith("VERTEX_TRACKXYZ"):
+            node_type = NodeType.LANDMARK
+        else:
+            raise Exception(
+                "Unhandled input node type {}".format(g2o_line.split()[0]))
+        key = int(g2o_line.split()[1])
+        pose = Pose3D(g2o_line)
+        return Node(key, node_type, pose)
 
 
-def edge_from_g2o_edge(g2o_line):
-    if line.startswith("EDGE_SE2"):
-        pass
-        return edge
+def edge_from_g2o_edge(g2o_line, is_3d):
+    if not is_3d:
+        kf = int(g2o_line.split()[1])
+        kt = int(g2o_line.split()[2])
+        if g2o_line.split()[0] == "EDGE_SE2":
+            if kt == kf + 1:
+                edge_type = EdgeType.ODOM
+            else:
+                edge_type = EdgeType.LOOP_CLOSURE
+        elif g2o_line.split()[0] == "EDGE_SE2_XY":
+            edge_type = EdgeType.LANDMARK
+        else:
+            raise Exception(
+                "Unhandled input edge type {}".format(g2o_line.split()[0]))
+        return Edge(kf, kt, edge_type)
+    else:
+        kf = int(g2o_line.split()[1])
+        kt = int(g2o_line.split()[2])
+        if g2o_line.split()[0] == "EDGE_SE3:QUAT":
+            if kt == kf + 1:
+                edge_type = EdgeType.ODOM
+            else:
+                edge_type = EdgeType.LOOP_CLOSURE
+        elif g2o_line.split()[0] == "EDGE_SE3_TRACKXYZ":
+            edge_type = EdgeType.LANDMARK
+        else:
+            raise Exception(
+                "Unhandled input edge type {}".format(g2o_line.split()[0]))
+        return Edge(kf, kt, edge_type)
 
-    if line.startswith("EDGE_SE3"):
-        pass
-        return edge
 
-
-def read_traj_from_g2o(filename):
+def read_pose_graph_from_g2o(filename, is_3d):
     if not os.path.exists(filename):
         print("Cannot find {}. ".format(filename))
         return []
@@ -80,10 +150,10 @@ def read_traj_from_g2o(filename):
 
     for line in f:
         if line.startswith("VERTEX"):
-            node = node_from_g2o_vertex(line)
+            node = node_from_g2o_vertex(line, is_3d)
             pose_graph.add_node(node)
         if line.startswith("EDGE"):
-            edge = edge_from_g2o_edge(g2o_line)
+            edge = edge_from_g2o_edge(line, is_3d)
             pose_graph.add_edge(edge)
 
     return pose_graph

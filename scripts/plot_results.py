cd91368d27ea7088964c89d2be34429f1183744b
yunchang <yunchang@mit.edu>
2024-03-20 09:47:30 -0400

    update visualization

---
+++ scripts/plot_results.py
@@ -0,0 +1,43 @@
+import utils
+import viz_utils
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import argparse
+
+est_color_mapping = {utils.NodeType.LANDMARK: "red",
+                     utils.EdgeType.ODOM: "blue"}
+ref_color_mapping = {utils.NodeType.LANDMARK: "grey",
+                     utils.EdgeType.ODOM: "grey"}
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Plot and compare results.")
+    parser.add_argument("input", help="input result folder")
+    parser.add_argument("reference", help="reference g2o")
+    parser.add_argument('-e', '--eval_list', nargs='+', default=[])
+    parser.add_argument('-l', '--label', nargs='+', default=[])
+    parser.add_argument('--is_3d', action='store_true')
+    parser.add_argument('--is_2d', dest='is_2d', action='store_false')
+    parser.set_defaults(is_3d=False)
+    args = parser.parse_args()
+
+    fig = plt.figure()
+    ref_pose_graph = utils.read_pose_graph_from_g2o(args.reference, args.is_3d)
+    for i in range(len(args.eval_list)):
+        input_g2o = "{}/{}/result.g2o".format(args.input, args.eval_list[i])
+        pose_graph = utils.read_pose_graph_from_g2o(input_g2o, args.is_3d)
+        if args.is_3d:
+            ax = fig.add_subplot(1, len(args.eval_list), i+1, projection="3d")
+            viz_utils.visualize_graph_3d(ax, pose_graph, est_color_mapping)
+            viz_utils.visualize_graph_3d(ax, ref_pose_graph, ref_color_mapping)
+        else:
+            ax = fig.add_subplot(1, len(args.eval_list), i+1)
+            viz_utils.visualize_graph_2d(ax, pose_graph, est_color_mapping)
+            viz_utils.visualize_graph_2d(ax, ref_pose_graph, ref_color_mapping)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/viz_utils.py
@@ -0,0 +1,68 @@
+import utils
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+def visualize_graph_2d(ax, graph, color_mapping):
+    nodes_xy = []
+    edges_xy = []
+    nodes_colors = []
+    edges_colors = []
+    for node in graph.nodes.values():
+        # Only visualize desired types
+        if node.type not in color_mapping:
+            continue
+        nodes_xy.append([node.pose.x, node.pose.y])
+        nodes_colors.append(color_mapping[node.type])
+
+    for edge in graph.edges:
+        # Only visualize desired types
+        if edge.type not in color_mapping:
+            continue
+        node_from = graph.nodes[edge.key_from]
+        node_to = graph.nodes[edge.key_to]
+        edges_xy.append(([node_from.pose.x, node_from.pose.y], [
+                        node_to.pose.x, node_to.pose.y]))
+        edges_colors.append(color_mapping[edge.type])
+
+    nodes_xy = np.asarray(nodes_xy)
+    edges_xy = np.asarray(edges_xy)
+
+    ax.scatter(nodes_xy[:, 0], nodes_xy[:, 1], c=nodes_colors)
+    for i in range(edges_xy.shape[0]):
+        ax.plot(edges_xy[i, :, 0], edges_xy[i, :, 1], color=edges_colors[i])
+
+
+def visualize_graph_3d(graph):
+    nodes_t = []
+    edges_t = []
+    nodes_colors = []
+    edges_colors = []
+    for node in graph.nodes.values():
+        # Only visualize desired types
+        if node.type not in color_mapping:
+            continue
+        nodes_t.append(node.pose.t)
+        nodes_colors.append(color_mapping[node.type])
+
+    for edge in graph.edges:
+        # Only visualize desired types
+        if edge.type not in color_mapping:
+            continue
+        node_from = graph.nodes[edge.key_from]
+        node_to = graph.nodes[edge.key_to]
+        edges_t.append((node_from.pose.t, node_to.pose.t))
+        edges_colors.append(color_mapping[edge.type])
+
+    nodes_t = np.asarray(nodes_t)
+    edges_t = np.asarray(edges_t)
+
+    ax.scatter(nodes_t[:, 0], nodes_t[:, 1], nodes_t[:, 2], c=nodes_colors)
+    for i in range(edges_t.shape[0]):
+        ax.plot3D(edges_t[i, :, 0], edges_t[i, :, 1],
+                  edges_t[i, :, 2], color=edges_colors[i])
+
+
+if __name__ == "__main__":
+    main()
